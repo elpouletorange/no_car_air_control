af895cca5e3e12764e18b2461474517e8cf2257e
elpouletorange <124886095+elpouletorange@users.noreply.github.com>
2024-10-02 22:58:44 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Google result'
+description 'Cant control car in air'
+version '1.0.0'
+
+client_script 'client.lua'
+
+dependency 'qb-minigames'
