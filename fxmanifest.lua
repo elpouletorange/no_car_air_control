5f2fda5589f58b7ad54de723928c1e8e5582e373
elpouletorange <124886095+elpouletorange@users.noreply.github.com>
2024-10-02 23:00:11 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes'
+
 author 'Google result'
 description 'Cant control car in air'
 version '1.0.0'
 
 client_script 'client.lua'
-
-dependency 'qb-minigames'
